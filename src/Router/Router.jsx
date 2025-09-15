4380606679d83957b01b62aeaad0f256726e0f94
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-09-16 00:47:42 +0600

    first commit

---
+++ src/Router/Router.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+export  const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+
